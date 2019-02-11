6c4c0502dbfa5eba250b3ef2b0dd8ea9bf1106a3
Alexander Nathanail <adnathanail@hotmail.co.uk>
2019-02-11 21:33:32 +0000

    Project 04

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -11,4 +11,53 @@
 // "white" in every pixel;
 // the screen should remain fully clear as long as no key is pressed.
 
-// Put your code here.
+// Initialise screen colour as white
+    @R2
+    M=0
+// Keep checking the keyboard input and, if the input is greater than 0, jump to FILL
+(LOOP)
+    @KBD
+    D=M
+    @FILLBLACK
+    D;JGT
+
+    @R2
+    D=M
+    @FILLWHITE
+    D; JLT
+
+    @LOOP
+    0;JMP
+
+(FILLBLACK)
+    @R2
+    M=-1
+    @FILL
+    0;JMP
+(FILLWHITE)
+    @R2
+    M=0
+    @FILL
+    0;JMP
+(FILL)
+    // Reset R1 to start of screen
+    @SCREEN
+    D=A
+    @R1
+    M=D-1
+(FILLLOOP)
+    @R2
+    D=M
+    @R1
+    M=M+1
+    A=M
+    M=D
+    @R1
+    D=M
+    @24575 // Last screen memory location
+    D=A-D
+    // If we are at the end of the screen return to the loop
+    @LOOP
+    D;JEQ
+    @FILLLOOP
+    0;JMP
--- projects/04/mult/mult.asm
+++ projects/04/mult/mult.asm
@@ -6,4 +6,31 @@
 // Multiplies R0 and R1 and stores the result in R2.
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
-// Put your code here.
+    // Initialise sum and tally
+    @R2 // Sum
+    M=0
+    @R3 // Tally
+    M=0
+
+(LOOP)
+    // Go to end if Tally==R0 i.e. multiplcation complete
+    @R0
+    D=M
+    @R3
+    D=D-M
+    @END
+    D;JLE
+
+    // Add R1 to total and go back to loop
+    @R1
+    D=M
+    @R2 // Total
+    M=M+D
+    @R3 // Tally
+    M=M+1
+    @LOOP
+    0;JMP
+
+(END)
+    @END
+    0;JMP
