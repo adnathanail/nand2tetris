fb41b402a9031f3da991f799009996a1fa127866
Alex Nathanail <adnathanail@hotmail.co.uk>
2024-03-06 15:19:41 +0000

    Project 6 V2

--- projects/06/assembler.py
+++ projects/06/assembler.py
@@ -31,50 +31,56 @@ JUMP_OPERATION_LOOKUP = {
 }
 
 with open("projects/06/add/Add.asm") as f:
-    x = f.read()
-    for row in x.split("\n"):
-        # Ignore empty rows
-        if row == "":
-            continue
-        # Ignore comments
-        if row[:2] == "//":
-            continue
-        # A instruction
-        if row[0] == "@":
-            out = "0"  # A starts with 0
-            val_dec = int(row[1:])  # Value to be loaded into A-register in base 10
-            val_binstr = bin(val_dec)[2:]  # Value in base 2
-            padding = (15 - len(val_binstr))*"0"  # To make the resulting binary 15-bits (16 inc first 0)
-            out += padding + val_binstr
-        # C instruction
-        else:
-            out = "111"  # C starts with 1, next 2 bits aren't used
-            out += "0"  # a
-            if ";" in row:
-                assignment, jump = row.split(";")
-            else:
-                assignment = row
-                jump = ""
-            destination, comparison = assignment.split("=")
-            
-            compbits = COMPARISON_OPERATION_LOOKUP[comparison]
-            
-            destbits = ""
-            if "A" in destination:
-                destbits += "1"
-            else:
-                destbits += "0"
-            if "D" in destination:
-                destbits += "1"
-            else:
-                destbits += "0"
-            if "M" in destination:
-                destbits += "1"
-            else:
-                destbits += "0"
+    source_assembly = f.read()
 
-            jumpbits = JUMP_OPERATION_LOOKUP[jump]
-            # print(destination, comparison, jump)
-            out += compbits + destbits + jumpbits
-            print(out)
-        print(out)
+
+compiled_hack_code = []
+for row in source_assembly.split("\n"):
+    # Ignore empty rows
+    if row == "":
+        continue
+    # Ignore comments
+    if row[:2] == "//":
+        continue
+    # A instruction
+    if row[0] == "@":
+        out = "0"  # A starts with 0
+        val_dec = int(row[1:])  # Value to be loaded into A-register in base 10
+        val_binstr = bin(val_dec)[2:]  # Value in base 2
+        padding = (15 - len(val_binstr))*"0"  # To make the resulting binary 15-bits (16 inc first 0)
+        out += padding + val_binstr
+    # C instruction
+    else:
+        out = "111"  # C starts with 1, next 2 bits aren't used
+        out += "0"  # a
+        if ";" in row:
+            assignment, jump = row.split(";")
+        else:
+            assignment = row
+            jump = ""
+        destination, comparison = assignment.split("=")
+        
+        compbits = COMPARISON_OPERATION_LOOKUP[comparison]
+        
+        destbits = ""
+        if "A" in destination:
+            destbits += "1"
+        else:
+            destbits += "0"
+        if "D" in destination:
+            destbits += "1"
+        else:
+            destbits += "0"
+        if "M" in destination:
+            destbits += "1"
+        else:
+            destbits += "0"
+
+        jumpbits = JUMP_OPERATION_LOOKUP[jump]
+
+        out += compbits + destbits + jumpbits
+    print(out)
+    compiled_hack_code.append(out + "\n")
+
+with open("projects/06/add/Add.hack", "w") as f:
+    f.writelines(compiled_hack_code)
