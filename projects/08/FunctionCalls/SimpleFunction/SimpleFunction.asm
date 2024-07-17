f3192c00b4ca50e812b6509aac35869532d471d9
Alex Nathanail <adnathanail@hotmail.co.uk>
2024-07-17 15:44:28 +0100

    Project 08 SimpleFunction V0

---
+++ projects/08/FunctionCalls/SimpleFunction/SimpleFunction.asm
@@ -0,0 +1,124 @@
+// SP = 256
+@256
+D=A
+@SP
+M=D
+// LCL = 300
+@300
+D=A
+@LCL
+M=D
+// ARG = 400
+@400
+D=A
+@ARG
+M=D
+// THIS = 3000
+@3000
+D=A
+@THIS
+M=D
+// THAT = 3010
+@3010
+D=A
+@THAT
+M=D
+// function SimpleFunction.test 2
+// push local 0
+@LCL
+D=M
+@0
+D=D+A
+A=D
+D=M
+@SP
+A=M
+M=D
+@SP
+M=M+1
+// push local 1
+@LCL
+D=M
+@1
+D=D+A
+A=D
+D=M
+@SP
+A=M
+M=D
+@SP
+M=M+1
+// add
+@SP
+M=M-1
+A=M
+D=M
+@SP
+M=M-1
+A=M
+M=M+D
+@SP
+M=M+1
+// not
+@SP
+M=M-1
+A=M
+M=!M
+@SP
+M=M+1
+// push argument 0
+@ARG
+D=M
+@0
+D=D+A
+A=D
+D=M
+@SP
+A=M
+M=D
+@SP
+M=M+1
+// add
+@SP
+M=M-1
+A=M
+D=M
+@SP
+M=M-1
+A=M
+M=M+D
+@SP
+M=M+1
+// push argument 1
+@ARG
+D=M
+@1
+D=D+A
+A=D
+D=M
+@SP
+A=M
+M=D
+@SP
+M=M+1
+// sub
+@SP
+M=M-1
+A=M
+D=M
+@SP
+M=M-1
+A=M
+M=M-D
+@SP
+M=M+1
+// return
+@SP
+M=M-1
+A=M
+D=M
+@SP
+M=M-1
+A=M
+@SP
+M=M+1
