4c89ffb2b729e4f2185126ff526a0f06dfcf14f0
Alex Nathanail <7809723+adnathanail@users.noreply.github.com>
2025-05-02 12:44:55 +1000

    Project 10 V3.1 refactor CompilationEngine to have functions for 3/5 token types

--- projects/10/CompilationEngine.py
+++ projects/10/CompilationEngine.py
@@ -7,37 +7,44 @@ class CompilationEngine:
         self.tokenizer = tokenizer
         self.output = ""
 
-    def compileClass(self):
+    def _parseKeyword(self, expectedKeyword):
         self.tokenizer.advance()
-        if (self.tokenizer.tokenType, self.tokenizer.token) == ("keyword", "class"):
-            print("<class>")
-            print(f"<keyword> {self.tokenizer.token} </keyword>")
+        if (self.tokenizer.tokenType, self.tokenizer.token) == ("keyword", expectedKeyword):
+            return f"<keyword> {self.tokenizer.token} </keyword>"
         else:
-            raise CompilationError(f"Expected keyword class, got {self.tokenizer.tokenType} {self.tokenizer.token}")
+            raise CompilationError(f"Expected keyword {expectedKeyword}, got {self.tokenizer.tokenType} {self.tokenizer.token}")
 
+    def _parseSymbol(self, expectedSymbol):
+        self.tokenizer.advance()
+        if (self.tokenizer.tokenType, self.tokenizer.token) == ("symbol", expectedSymbol):
+            return f"<symbol> {self.tokenizer.token} </symbol>"
+        else:
+            raise CompilationError(f"Expected symbol {expectedSymbol}, got {self.tokenizer.tokenType} {self.tokenizer.token}")
+
+    # parseIntegerConstant
+
+    # parseStringConstant
+
+    def _parseIdentifier(self):
         self.tokenizer.advance()
         if self.tokenizer.tokenType == "identifier":
-            print(f"<identifier> {self.tokenizer.token} </identifier>")
+            return f"<identifier> {self.tokenizer.token} </identifier>"
         else:
-            raise CompilationError(f"Expected className identifier, got {self.tokenizer.tokenType} {self.tokenizer.token}")
+            raise CompilationError(f"Expected identifier, got {self.tokenizer.tokenType} {self.tokenizer.token}")
 
-        self.tokenizer.advance()
-        if (self.tokenizer.tokenType, self.tokenizer.token) == ("symbol", "{"):
-            print("<identifier> { </identifier>")
-        else:
-            raise CompilationError(f"Expected symbol {{, got {self.tokenizer.tokenType} {self.tokenizer.token}")
+    def compileClass(self):
+        print("<class>")
+        print(self._parseKeyword("class"))
+        print(self._parseIdentifier())
+        print(self._parseSymbol("{"))
 
         # classVarDec*
 
         # subroutineDec*
 
-        self.tokenizer.advance()
-        if (self.tokenizer.tokenType, self.tokenizer.token) == ("symbol", "}"):
-            print("<identifier> } </identifier>")
-            print("</class>")
-        else:
-            raise CompilationEngine(f"Expected symbol }}, got {self.tokenizer.tokenType} {self.tokenizer.token}")
-        
+        print(self._parseSymbol("}"))
+        print("</class>")
+
         if self.tokenizer.hasMoreTokens():
             self.tokenizer.advance()
             raise CompilationError(f"Nothing expected after class definition, got {self.tokenizer.tokenType} {self.tokenizer.token}")
